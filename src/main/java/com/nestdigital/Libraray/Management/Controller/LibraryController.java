3469fee56cfdbed43ff06858e153e87b93b7bb4f
Ansen123 <ansenfp@gmail.com>
2022-10-20 10:18:58 +0530

    first commit

--- src/main/java/com/nestdigital/Libraray/Management/Controller/LibraryController.java
+++ src/main/java/com/nestdigital/Libraray/Management/Controller/LibraryController.java
@@ -5,6 +5,7 @@ import com.nestdigital.Libraray.Management.Model.LibraryModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
@@ -19,7 +20,19 @@ public class LibraryController {
     }
     @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public List<LibraryModel>viewlibrary(){
-        return (List<LibraryModel>)dao.findAll();
+    public List<LibraryModel>viewlibrary() {
+        return (List<LibraryModel>) dao.findAll();
+
     }
+        @CrossOrigin(origins = "*")
+        @PostMapping("/delete")
+        @Transactional
+        public String deletebook(@RequestBody LibraryModel library){
+        dao.deleteBookById(library.getId());
+        return "{status:Success}";
+
+        }
+
+
+
 }
--- src/main/java/com/nestdigital/Libraray/Management/Dao/LibraryDao.java
+++ src/main/java/com/nestdigital/Libraray/Management/Dao/LibraryDao.java
@@ -1,8 +1,14 @@
 package com.nestdigital.Libraray.Management.Dao;
 
 import com.nestdigital.Libraray.Management.Model.LibraryModel;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface LibraryDao extends CrudRepository<LibraryModel,Integer> {
+@Modifying
+    @Query(value = "DELETE FROM `library` WHERE `id`= :id",nativeQuery = true)
+    void deleteBookById(Integer id);
+
 
 }
